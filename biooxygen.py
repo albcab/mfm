very rough draft of structured/bayesian flows for biooxygen and predator_prey these work decently in biooxygen and not really on predator-prey fixes an mistake on the distribution calculation of predator-prey makes some necessary restructuring on flows and introduces taylor made flows from here I take a different direction, that of simulation-based learning

biooxygen.py
@@ -4,9 +4,13 @@ import jax
 import jax.numpy as jnp
 import jax.random as jrnd
 import optax
+import distrax as dx
+import haiku as hk
 
 from distributions import BioOxygen
 from execute import run
+from exe import taylor_run
+from flows import Flow, RealSufficient, PositiveSufficient, make_cond, shift_scale
 
 from jax.config import config
 # config.update("jax_debug_nans", True)
@@ -14,6 +18,75 @@ config.update("jax_enable_x64", True)
 
 
 N_PARAM = 2
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def plots(samples, param, flow, flow_inv):#, sam, sam2):
+    np.random.seed(0)
+    c, n = samples["x1"].shape
+    u1 = np.random.normal(0., 1., size=c * n)
+    u2 = np.random.normal(0., 1., size=c * n)
+    x1 = samples["x1"].reshape(-1)
+    x2 = samples["x2"].reshape(-1)
+    phi_samples, phi_weights = jax.vmap(lambda u1, u2: flow(jnp.array([u1, u2]), param))(u1, u2)
+    pi_samples, pi_weights = jax.vmap(lambda x1, x2: flow_inv(jnp.array([x1, x2]), param))(x1, x2)
+    w = jnp.exp(phi_weights)
+    print(jnp.min(w), jnp.max(w))
+    w = jnp.exp(pi_weights)
+    print(jnp.min(w), jnp.max(w))
+
+    fig, ax = plt.subplots(1, 2, figsize=(11, 4), sharex=True, sharey=True)
+    ax[1].set_title(r"$\hat{\phi}(\theta)$")
+    ax[1].set_xlabel(r"$\theta_1$")
+    ax[1].set_ylabel(r"$\theta_2$")
+    # sns.kdeplot(x=phi_samples[:, 0], y=phi_samples[:, 1], ax=ax[2], fill=True)
+    sns.histplot(x=phi_samples[:, 0], y=phi_samples[:, 1], weights=jnp.exp(phi_weights), ax=ax[1], bins=50)
+    ax[0].set_title(r"$\pi(\theta)$")
+    ax[0].set_xlabel(r"$\theta_1$")
+    ax[0].set_ylabel(r"$\theta_2$")
+    sns.histplot(x=x1, y=x2, ax=ax[0], bins=50)
+    # ax[0].sharex(ax[1])
+    # ax[0].sharey(ax[1])
+    # plt.savefig("biooxygen2.png", bbox_inches='tight')
+    # plt.close()
+
+    return None
+
+class Message(Flow):
+    def __init__(self, obs, times, num_bins=False, hidden_dims=0, non_linearity=None):
+        super().__init__(num_bins, hidden_dims, non_linearity)
+        self.obs = obs
+        self.times = times
+
+    def flows(self):
+        # t0_transform = lambda x: self.obs / (1 - jnp.exp(-x * self.times))
+        t0_transform = lambda x: jnp.array([jnp.mean(self.obs), jnp.mean(self.times)])
+        t1_transform = lambda x: jnp.array([x[0], jnp.mean(self.obs), jnp.mean(self.times)])
+        # t1_transform = lambda x: -jnp.log(1 - self.obs / x) / self.times
+
+        flows = []
+        t0_1 = make_cond(1, RealSufficient(axis=1, transform=t0_transform), self)
+        flows.append(dx.SplitCoupling(1, 1, t0_1, self.coupling_fn, swap=True))
+
+        t1_1 = make_cond(1, RealSufficient(axis=1, transform=t1_transform), self)
+        flows.append(dx.SplitCoupling(1, 1, t1_1, self.coupling_fn, swap=False))
+
+        # t0_2 = make_cond(1, RealSufficient(axis=None, transform=t0_transform), self)
+        # flows.append(dx.SplitCoupling(1, 1, t0_2, self.coupling_fn, swap=True))
+
+        # t1_2 = make_cond(1, RealSufficient(axis=None, transform=t1_transform), self)
+        # flows.append(dx.SplitCoupling(1, 1, t1_2, self.coupling_fn, swap=False))
+
+        # t0_3 = make_cond(1, RealSufficient(axis=None, transform=t0_transform), self)
+        # flows.append(dx.SplitCoupling(1, 1, t0_3, self.coupling_fn, swap=True))
+
+        # t1_3 = make_cond(1, RealSufficient(axis=None, transform=t1_transform), self)
+        # flows.append(dx.SplitCoupling(1, 1, t1_3, self.coupling_fn, swap=False))
+
+        return dx.Chain(flows)
 
 def main(args):
 
@@ -34,7 +107,14 @@ def main(args):
         transition_steps=n_warm-10, decay_rate=.1, transition_begin=10)
     optim = optax.adam(schedule)
 
-    run(dist, args, optim, N_PARAM, batch_fn=jax.vmap)
+    particles, *plot_params = run(dist, args, optim, N_PARAM, batch_fn=jax.vmap)
+    plots(*plot_params)
+    plots(particles, *(plot_params[1:]))
+
+    flow = Message(obs, times, num_bins=20)
+    plot_params = taylor_run(dist, args, flow, optim, N_PARAM, jax.vmap)
+    plots(*plot_params)
+    plt.show()
 
 
 if __name__ == "__main__":

distributions.py
@@ -144,6 +144,7 @@ class ProbitReg(Distribution):
         }
 
 
+from jax.experimental.ode import odeint
 class PredatorPrey:
     def __init__(self, 
         time,
@@ -164,16 +165,22 @@ class PredatorPrey:
             ),
         )
 
+        N = self.time.shape[0]
+        # ts = jnp.arange(float(N))
+        # z = odeint(dz_dt, z_init, ts, theta, rtol=1e-6, atol=1e-5, mxstep=1000)
+        
         term = diffrax.ODETerm(dz_dt)
-        # solver = diffrax.Dopri5()
+        solver = diffrax.Dopri5()
         # solver = diffrax.Tsit5()
         # solver = diffrax.Dopri8()
-        solver = diffrax.Heun()
+        # solver = diffrax.Heun()
+        saveat = diffrax.SaveAt(ts=[i+1 for i in range(N)])
         z = diffrax.diffeqsolve(
             term, solver, 
-            t0=0, t1=self.time.shape[0], dt0=1, 
+            t0=0, t1=N, dt0=1.,
             stepsize_controller=diffrax.PIDController(rtol=1e-6, atol=1e-5),
             y0=z_init, args=theta,
+            saveat=saveat,
             max_steps=1000,
             throw=False,
         ).ys

flows.py
@@ -1,20 +1,21 @@
 import abc
-from typing import Callable
+from typing import Callable, Optional
 
+import jax
 import jax.numpy as jnp
 from jax.flatten_util import ravel_pytree
-
+from jax.experimental.host_callback import id_print
 
 import haiku as hk
 import distrax as dx
 
 
 def affine_coupling(params):
-    return dx.ScalarAffine(shift=params[0], log_scale=params[1])
+    return dx.ScalarAffine(shift=params[:, 0], log_scale=params[:, 1])
 
 def rquad_spline_coupling(params):
     return dx.RationalQuadraticSpline(
-        params, range_min=-6., range_max=6.)
+        params, range_min=-3., range_max=3.)
 
         
 def make_dense(d, hidden_dims, norm, non_linearity, num_bins):
@@ -31,12 +32,81 @@ def make_dense(d, hidden_dims, norm, non_linearity, num_bins):
     else:
         layers.extend([
             hk.Linear(2 * d, w_init=hk.initializers.VarianceScaling(.01), b_init=hk.initializers.RandomNormal(.01)),
-            hk.Reshape((2, d), preserve_dims=-1)
+            hk.Reshape((d, 2), preserve_dims=-1)
         ])
     return hk.Sequential(layers)
 
 
+def make_cond(out_dim, first_layer, self, batch=False):
+    layers = [first_layer]
+    n_param = 3 * self.num_bins + 1 if self.num_bins else 2
+    for _ in range(self.hidden_dims):
+        layers.append(hk.Linear(n_param * out_dim))
+        layers.append(self.non_linearity)
+    if batch:
+        layers.append(BatchLinear(n_param))
+    else:
+        layers.append(hk.Linear(n_param * out_dim))
+        layers.append(hk.Reshape((out_dim, n_param), preserve_dims=-1))
+    return hk.Sequential(layers)
+
+
+class BatchLinear(hk.Module):
+    def __init__(self, n_param, name=None):
+        super().__init__(name)
+        self.n_param = n_param
+
+    def __call__(self, x):
+        return jax.vmap(lambda x: hk.Linear(self.n_param)(x))(x)
+
+
+class Sufficient(hk.Module):
+    def __init__(self, transform, axis=None, name=None):
+        super().__init__(name)
+        self.axis = axis
+        self.transform = transform
+
+class RealSufficient(Sufficient):
+    def __call__(self, x):
+        x = self.transform(x)
+        x = (x - x.mean()) / x.std()
+        return x
+        first = jnp.sum(x, axis=self.axis)
+        second = jnp.sum(x**2, axis=self.axis)
+        third = jnp.sum(x**3, axis=self.axis)
+        fourth = jnp.sum(x**4, axis=self.axis)
+        reg = jnp.array([first, second, third, fourth])
+        if self.axis is not None:
+            reg = ((reg - reg.mean(axis=0)) / reg.std(axis=0)).T
+        else:
+            reg = (reg - reg.mean()) / reg.std()
+        return reg
+
+class PositiveSufficient(Sufficient):
+    def __call__(self, log_x):
+        log_x = self.transform(log_x)
+        logfirst = jnp.sum(log_x, axis=self.axis)
+        nfirst = jnp.sum(-jnp.exp(log_x), axis=self.axis)
+        nsecond = jnp.sum(-jnp.exp(log_x)**2, axis=self.axis)
+        nthird = jnp.sum(-jnp.exp(log_x)**3, axis=self.axis)
+        reg = jnp.array([logfirst, nfirst, nsecond, nthird])
+        if self.axis is not None:
+            reg = ((reg - reg.mean(axis=0)) / reg.std(axis=0)).T
+        else:
+            reg = (reg - reg.mean()) / reg.std()
+        return reg
+
+
 class Flow(metaclass=abc.ABCMeta):
+
+    def __init__(self, num_bins, hidden_dims, non_linearity):
+        if num_bins:
+            self.coupling_fn = affine_coupling
+        else:
+            self.coupling_fn = affine_coupling
+        self.num_bins = num_bins
+        self.hidden_dims = hidden_dims
+        self.non_linearity = non_linearity
 
     @abc.abstractmethod
     def flows(self):
@@ -66,17 +136,11 @@ class Coupling(Flow):
         hidden_dims: int, non_linearity: Callable, norm: bool,
         num_bins: int = None,
     ):
-        if num_bins:
-            self.coupling_fn = rquad_spline_coupling
-        else:
-            self.coupling_fn = affine_coupling
+        super().__init__(num_bins, hidden_dims, non_linearity)
         self.split = int(d/2 + .5)
         self.d = d
         self.n_flow = n_flow
-        self.hidden_dims = hidden_dims
-        self.non_linearity = non_linearity
         self.norm = norm
-        self.num_bins = num_bins
 
     def flows(self):
         flows = []

german_credit.py
@@ -4,14 +4,80 @@ import pandas as pd
 import numpy as np
 
 import jax
+import jax.numpy as jnp
 import optax
+import distrax as dx
+import haiku as hk
 
 from distributions import HorseshoeLogisticReg, ProbitReg
 from execute import run
+from exe import taylor_run
+from flows import Flow, RealSufficient, PositiveSufficient, make_cond
 
 from jax.config import config
 # config.update("jax_debug_nans", True)
 config.update("jax_enable_x64", True)
+
+from jax.experimental.host_callback import id_print
+
+class HorseshoeSufficient(hk.Module):
+    def __call__(self, log_input):
+        log_x = log_input[:-1] + log_input[-1]
+        logfirst = log_x
+        nfirst = -jax.numpy.exp(log_x)
+        nsecond = -jax.numpy.exp(log_x)**2
+        nthird = -jax.numpy.exp(log_x)**3
+        reg = jax.numpy.vstack([logfirst, nfirst, nsecond, nthird])
+        reg = ((reg - reg.mean(axis=0)) / reg.std(axis=0)).T
+        return reg
+
+class Message(Flow):
+
+    def __init__(self, y, X, num_bins=False, hidden_dims=0, non_linearity=None): 
+        super().__init__(num_bins, hidden_dims, non_linearity)
+        self.y = y
+        self.X = X
+        _, j = X.shape
+        d = j * 2 + 1
+        false = [False] * j
+        true = [True] * j
+        self.tau_mask = np.array(true + true + [False])
+        self.lambda_mask = np.array(true + false + [True])
+        self.beta_mask = np.array(false + true + [True])
+        self.d = d
+        self.j = j
+
+    def flows(self):
+        flows = []
+        tau_prior_layer = PositiveSufficient(axis=None, transform=lambda _: jnp.log(1))
+        tau_prior = make_cond(self.d, tau_prior_layer, self)
+        flows.append(dx.MaskedCoupling(self.tau_mask, tau_prior, self.coupling_fn))
+        
+        lambda_prior_layer = PositiveSufficient(axis=None, transform=lambda _: jnp.log(1))        
+        lambda_prior = make_cond(self.d, lambda_prior_layer, self)
+        flows.append(dx.MaskedCoupling(self.lambda_mask, lambda_prior, self.coupling_fn))
+        
+        lambda_tau = lambda log_x: jnp.atleast_2d(log_x[:-1] + log_x[-1])
+        beta_prior_layer = PositiveSufficient(axis=0, transform=lambda_tau)
+        beta_prior = make_cond(self.j, beta_prior_layer, self, batch=True)
+        flows.append(dx.SplitCoupling(self.j, 1, beta_prior, self.coupling_fn, swap=True))
+
+        #what is the best use for the first moment? OLS?
+        data = lambda _: self.y * self.X.T
+        beta_lik_layer = RealSufficient(axis=1, transform=data)
+        beta_lik = make_cond(self.j, beta_lik_layer, self, batch=True)
+        flows.append(dx.SplitCoupling(self.j, 1, beta_lik, self.coupling_fn, swap=True))
+
+        beta = lambda x: jnp.atleast_2d(jnp.concatenate([x[:self.j], x[:self.j], x[:1]]))
+        lambda_lik_layer = RealSufficient(axis=0, transform=beta)
+        lambda_lik = make_cond(self.d, lambda_lik_layer, self, batch=True)
+        flows.append(dx.MaskedCoupling(self.lambda_mask, lambda_lik, self.coupling_fn))
+
+        tau_lik_layer = RealSufficient(axis=None, transform=beta)
+        tau_lik = make_cond(self.d, tau_lik_layer, self)
+        flows.append(dx.MaskedCoupling(self.tau_mask, tau_lik, self.coupling_fn))
+
+        return dx.Chain(flows)
 
 
 def main(args):
@@ -34,7 +100,10 @@ def main(args):
     print("\n\nSetting up German credit logistic horseshoe model...")
     dist = HorseshoeLogisticReg(X, y)
 
-    run(dist, args, optim, N_PARAM, batch_fn=jax.vmap)
+    # run(dist, args, optim, N_PARAM, batch_fn=jax.vmap)
+
+    flow = Message(y, X)
+    taylor_run(dist, args, flow, optim, N_PARAM, jax.vmap)
 
 
 if __name__ == "__main__":